7395490bf1a6b048c6a84db1517d2d08af8063e5
Akhil mk <akhilmk@intertoons.com>
2026-05-30 18:12:22 +0530

    Delete .vercel directory

--- .vercel/output/static/_worker.js/__next-on-pages-dist__/webpack/99445.js
+++
@@ -1,1 +0,0 @@
-var t={},_=(o,c,r)=>(t.__chunk_99445=(s,l,e)=>{"use strict";e.d(l,{A:()=>a});let a=(0,e(38734).A)("globe",[["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}],["path",{d:"M12 2a14.5 14.5 0 0 0 0 20 14.5 14.5 0 0 0 0-20",key:"13o1zl"}],["path",{d:"M2 12h20",key:"9i4pu4"}]])},t);export{_ as __getNamedExports};
